33aff39961f65e0d2027ad6ddf96b5939a8f95c2
AweiWoo <724862897@qq.com>
2022-04-19 08:58:40 +0800

    20220419

--- LogModule/fileconfig/logger_fileconfig.py
+++ LogModule/fileconfig/logger_fileconfig.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 # Author : wwu
 
+#使用fileConfig配置文件来定义日志格式
+
 from asyncio.log import logger
 import logging
 import logging.config
 
 logging.config.fileConfig('logging.conf')
-
 logger = logging.getLogger('simple')
 
 logger.debug('debug message')
 logger.info('info message')
 logger.error('error message')
-logger.warn('warn message')
+logger.warning('warn message')
 logger.critical('critical message')
--- LogModule/logger3.py
+++ LogModule/logger3.py
@@ -5,16 +5,8 @@
 import logging
 
 def third():
-    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(user)s[%(ip)s] - %(message)s"
-    DATE_FORMAT = "%m/%d/%y %H:%M:%S %p"
-
-    logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT)
-    logging.warning("this is third log.", exc_info=True, stack_info=True, extra={'user': 'Tom', 'ip':'192.168.1.2'})
-
-third()
-
-#结果：
-"""
+    """
+    结果：
     04/16/22 17:55:39 PM - WARNING - Tom[192.168.1.2] - this is third log.
     NoneType: None    #exe_info控制：
     Stack (most recent call last):   #stack_info控制
@@ -22,4 +14,11 @@ third()
         third()
     File "d:\Code\code-practise\LogModule\logger3.py", line 25, in third
         logging.warning("this is third log.", exc_info=True, stack_info=True, extra={'user': 'Tom', 'ip':'192.168.1.2'})
-"""
+    """
+    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(user)s[%(ip)s] - %(message)s"
+    DATE_FORMAT = "%m/%d/%y %H:%M:%S %p"
+
+    logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+    logging.warning("this is third log.", exc_info=True, stack_info=True, extra={'user': 'Tom', 'ip':'192.168.1.2'})
+
+third()
